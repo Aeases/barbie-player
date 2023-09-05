8d311eb81df238bf26621802eb4be25d9dd529ba
aeases <96164686+Aeases@users.noreply.github.com1~>
2023-09-05 12:12:16 +0800

    update add restart button

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -2,7 +2,7 @@ export default function PlayButton({isPlaying, classname}: {isPlaying: boolean, 
     if (isPlaying == true) {
         return(
             <>
-                <svg className={classname} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>
+                <svg className={classname} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>
             </>
         )
     }
@@ -10,7 +10,7 @@ export default function PlayButton({isPlaying, classname}: {isPlaying: boolean, 
     else {
         return(
             <>
-                 <svg className={classname} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>
+                <svg className={classname} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>
             </>
         )
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useState } from "react";
 import PlayButton from "y/components/PlayButton";
 export default function Home() {
-  const [audioPlaying, setAudioPlaying] = useState(false)
+  const [audioPlaying, setAudioPlaying] = useState(true)
   function playAudio() {
     const audio_reference = document.querySelector("#audio_id")
     if (audio_reference instanceof HTMLAudioElement) {
@@ -12,9 +11,17 @@ export default function Home() {
       console.log(audio_reference)
       console.log(audioPlaying)
       
+    }  
+  }
+
+  function restartAudio() {
+    const audio_reference = document.querySelector("#audio_id")
+    if (audio_reference instanceof HTMLAudioElement) {
+      //audio_reference.pause()
+      audio_reference.currentTime = 0
+      setAudioPlaying(true)
+      audio_reference.pause()
     }
-    
-    
   }
   
   return (
@@ -26,7 +33,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-[--background-primary-obsidian]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 ">
+          <div className="grid grid-cols-1 gap-16 sm:grid-cols-2 md:gap-16 ">
             <div
               className="flex flex-col gap-4 rounded-xl border-style scale-[200%] p-2 w-full max-h-lg md:max-w-xl" onClick={playAudio}>
               <h3 className="text-2xl font-bold text-white"> <PlayButton classname="transition-all" isPlaying={audioPlaying} /> </h3>
@@ -34,6 +41,13 @@ export default function Home() {
                 <a href="AudioTest.ogg">Download OGG audio</a>.
               </audio>
             </div>
+            <div
+              className="flex flex-col gap-4 rounded-xl border-style scale-[200%] p-2 w-full max-h-lg md:max-w-xl" onClick={restartAudio}>
+              <h3 className="text-2xl font-bold text-white"> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/><path d="M3 3v5h5"/></svg>
+              </h3>
+            </div>
+
           </div>
         </div>
       </main>
