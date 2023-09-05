8449883abe66704ac19d2e6ba3091a8ae85f461e
aeases <96164686+Aeases@users.noreply.github.com1~>
2023-09-05 11:59:10 +0800

    Initial again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
-import "y/styles/globals.css";
+import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
